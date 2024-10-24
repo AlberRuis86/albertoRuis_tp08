d36b6b6e059321a1a69fcff0f7981f7ff986fc31
Alberto Ruis <alber.ruis2886@gmail.com>
2024-10-24 00:19:53 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let menuVisible = false;
+//Se oculta o muestra el menu
+function mostrarOcultarMenu(){
+    if(menuVisible){
+        document.getElementById("nav").classList ="";
+        menuVisible = false;
+    }else{
+        document.getElementById("nav").classList ="responsive";
+        menuVisible = true;
+    }
+}
+
+function seleccionar(){
+    //Se oculta el menu una vez que selecciono
+    document.getElementById("nav").classList = "";
+    menuVisible = false;
+}
+//Se aplican animaciones de las skills
+function efectoHabilidades(){
+    var skills = document.getElementById("skills");
+    var distancia_skills = window.innerHeight - skills.getBoundingClientRect().top;
+    if(distancia_skills >= 300){
+        let habilidades = document.getElementsByClassName("progreso");
+        habilidades[0].classList.add("javascript");
+        habilidades[1].classList.add("htmlcss");
+        habilidades[2].classList.add("react");
+        habilidades[3].classList.add("figma");
+        habilidades[4].classList.add("comunicacion");
+        habilidades[5].classList.add("trabajo");
+        habilidades[6].classList.add("creatividad");
+        habilidades[7].classList.add("dedicacion");
+    }
+}
+
+
+//scrolling para aplicar la animacion de la barra de skills
+window.onscroll = function(){
+    efectoHabilidades();
+} 
